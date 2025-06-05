b57845c9b39be1c6730c75f2ce213b00113ef0f0
ferwirrow <ferkedezma95@gmail.com>
2025-06-05 12:24:31 -0600

    first boot.dev notes

---
+++ boot.dev/c/basics.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+
+int main() {
+    // sizeof nos permite obtener el size en bytes de el type de una variable
+    // Use %zu is for printing `sizeof` result
+  printf("El size de un int es: %zu\n", sizeof(int));
+}
